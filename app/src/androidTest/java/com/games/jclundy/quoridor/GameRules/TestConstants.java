bfaac3d6fb6d5b2b01b58b7f05456094c93b13e6
Joseph Lundy <joseph.lundy456@gmail.com>
2014-11-04 08:40:15 -0500

    adds constants for testing

--- app/src/androidTest/java/com/games/jclundy/quoridor/GameRules/TestConstants.java
+++ app/src/androidTest/java/com/games/jclundy/quoridor/GameRules/TestConstants.java
@@ -3,5 +3,22 @@ package com.games.jclundy.quoridor.GameRules;
 public class TestConstants {
     static final int[] ZERO_SET = {1, 9, 10};
     static final int[] EIGHTY_SET = {79,71, 70};
-    static final int [] FORTY_SET = {39, 41, 49, 31, 48, 50, 47, 49};
+    static final int [] FORTY_SET = {48, 49, 50, 39, 41, 30, 31, 32};
+    static final int [] FOUR_SET = {3, 5, 12, 13, 14};
 }
+
+/*        48  49 50
+          39  40 41
+          30  31 32
+
+
+
+         79 80
+         70 71
+
+         9 10
+         0 1
+
+        12 13 14
+        3 4 5
+ */
