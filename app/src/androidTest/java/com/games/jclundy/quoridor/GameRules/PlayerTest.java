5a922075465744c968d41941ba8113094e98436f
Joseph Lundy <joseph.lundy456@gmail.com>
2014-11-04 08:39:57 -0500

    implements player

--- app/src/androidTest/java/com/games/jclundy/quoridor/GameRules/PlayerTest.java
+++ app/src/androidTest/java/com/games/jclundy/quoridor/GameRules/PlayerTest.java
@@ -18,31 +18,57 @@ public class PlayerTest extends ApplicationTestCase<Application> {
         assertEquals(0, player.id);
         assertEquals(4, player.startPosition);
         assertEquals(10, player.chipsLeft);
-        assertEquals(0, player.moveHistory.size());
+        assertEquals(1, player.moveHistory.size());
     }
 
     public void testHasReachedEndZone(){
         Player player = zPlayer();
-        player.currentSquare = 13;
+        player.recordMove(13, false);
         assertFalse(player.hasReachedEndZone());
 
-        player.currentSquare = 72;
+        player.recordMove(80, false);
         assertTrue(player.hasReachedEndZone());
     }
 
+    public void testGetCurrentSquare(){
+        Player player = zPlayer();
+        int firstSquare = player.getLastMove().playerPosition;
+        assertEquals(4, firstSquare);
+
+    }
+
     public void testRecordMove(){
-        
+        int start = 0;
+        int chips = 10;
+        int id = 1;
+        Player player = new Player(id, start, chips);
+        player.recordMove(1, false);
+
+        assertEquals(0, player.startPosition);
+        assertEquals(1, player.getCurrentSquare());
+        assertEquals(2, player.moveHistory.size());
     }
 
     public void testGetLastMove(){
-
+        Player player = zPlayer();
+        player.recordMove(1, false);
+        assertEquals(1, player.getLastMove().playerPosition);
+        assertEquals(1, player.getCurrentSquare());
+        assertEquals(-1, player.getLastMove().chipPosition);
     }
 
-    public void testUndoLastMove(){
+    public void testUndoMove(){
+        Player player = zPlayer();
+        player.recordMove(13, false);
+        player.recordMove(22, false);
+        player.undoMove();
 
-    }
+        assertEquals(13, player.getCurrentSquare());
+        assertEquals(-1, player.getLastMove().chipPosition);
 
-    public void testMoveHistory(){
-
+        player.recordMove(40, true);
+        assertEquals(13, player.getCurrentSquare());
+        assertEquals(40, player.getLastMove().chipPosition);
+        assertEquals(9, player.getChipsLeft());
     }
 }
--- app/src/main/java/com/games/jclundy/quoridor/GameRules/Player.java
+++ app/src/main/java/com/games/jclundy/quoridor/GameRules/Player.java
@@ -3,13 +3,9 @@ package com.games.jclundy.quoridor.GameRules;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by devfloater65 on 10/22/14.
- */
 public class Player {
     int id;
     int startPosition;
-    int currentSquare;
     int chipsLeft;
     boolean hasWon;
     Stack moveHistory;
@@ -17,62 +13,79 @@ public class Player {
     Player(int playerID, int start, int chips){
         id = playerID;
         startPosition = start;
-        currentSquare = start;
         chipsLeft = chips;
         hasWon = false;
         moveHistory = new Stack();
+        Move firstMove = new Move(start);
+        moveHistory.push(firstMove);
     }
 
     public boolean hasReachedEndZone(){
         int startCol = startPosition % GameRuleConstants.NUM_COLS;
-        int currentCol = currentSquare % GameRuleConstants.NUM_COLS;
+        int currentCol = getCurrentSquare() % GameRuleConstants.NUM_COLS;
 
         int startRow = startPosition / GameRuleConstants.NUM_ROWS;
-        int currentRow = currentSquare / GameRuleConstants.NUM_ROWS;
+        int currentRow = getCurrentSquare()/ GameRuleConstants.NUM_ROWS;
 
         int colDiff = Math.abs(startCol - currentCol);
         int rowDiff = Math.abs(startRow - currentRow);
-        return colDiff == 9 || rowDiff == 9;
+        return colDiff == 8 || rowDiff == 8;
+    }
+
+    public int getCurrentSquare(){
+        return moveHistory.peek().playerPosition;
+    }
+
+    public Move getLastMove(){
+        return moveHistory.peek();
+    }
+
+    public void undoMove(){
+        Move lastMove = moveHistory.peek();
+        if(lastMove.chipPosition > -1)
+            chipsLeft ++;
+        moveHistory.pop();
+    }
+
+    public void recordMove(int square, boolean didPlaceChip){
+        Move latestMove;
+        if(didPlaceChip && chipsLeft >= 0) {
+            latestMove = new Move(getLastMove().playerPosition, square);
+            chipsLeft --;
+        }
+        else
+            latestMove = new Move(square);
+        moveHistory.push(latestMove);
+    }
+
+    public int getChipsLeft(){
+        return chipsLeft;
     }
 
     protected class Stack {
-        private int top;
+        private int count;
         private List<Move> stack;
         Stack(){
-            top = 0;
+            count = 0;
             stack = new ArrayList<Move>();
         }
-        void push(){
-
+        void push(Move move){
+            stack.add(move);
+            count++;
         }
         void pop(){
-
+            if(stack.size() > 1){
+                stack.remove(count - 1);
+                count--;
+            }
         }
 
         Move peek(){
-            if(stack.size() > 0)
-                return stack.get(top);
-            else
-                return null;
+            return stack.get(count - 1);
         }
 
         int size(){
             return stack.size();
-        }
-    }
-
-    protected class Move{
-        int playerPosition;
-        int chipPosition;
-
-        Move(int square){
-            playerPosition = square;
-            chipPosition = -1;
-        }
-
-        Move(int playerPosition, int chipPosition){
-            this.playerPosition = playerPosition;
-            this.chipPosition = chipPosition;
         }
     }
 }
