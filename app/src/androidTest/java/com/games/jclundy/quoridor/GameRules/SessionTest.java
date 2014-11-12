ace8e6c3905e6beb31450844273f130ace0bd13e
Joseph Lundy <joseph.lundy456@gmail.com>
2014-11-11 20:08:07 -0500

    implements some Session methods and tests

--- app/src/androidTest/java/com/games/jclundy/quoridor/GameRules/SessionTest.java
+++ app/src/androidTest/java/com/games/jclundy/quoridor/GameRules/SessionTest.java
@@ -11,40 +11,108 @@ public class SessionTest extends ApplicationTestCase<Application> {
 
     public void testInit(){
 
+        verifyInitialization(2);
+        verifyInitialization(3);
+        verifyInitialization(4);
+    }
+
+    private void verifyInitialization(int num){
+        Session session = new Session(num);
+        assertTrue(session.isTurnToMove(0));
+        assertEquals(num, session.numPlayers);
+        assertEquals(num, session.playerList.length);
+        for(int i = 0 ; i < session.playerList.length; i ++){
+            assertEquals(i, session.playerList[i].id);
+            assertEquals(GameRuleConstants.START_POSITIONS[i], session.playerList[i].getCurrentSquare());
+        }
+    }
+
+    public void testIsMoveValid(){
+
+        Session session = new Session(2);
+
+        assertTrue(false);
+    }
+
+    public void testIsTurnToMove(){
+        Session session = new Session(2);
+        int id1 = session.playerList[0].id;
+        int id2 = session.playerList[1].id;
+
+        assertTrue(session.isTurnToMove(id1));
+        assertFalse(session.isTurnToMove(id2));
+
+        session.makeMove(id1, 13);
+        assertFalse(session.isTurnToMove(id1));
+        assertTrue(session.isTurnToMove(id2));
+
+        Move move2 = new Move(67);
+        session.makeMove(id2, move2);
+        assertFalse(session.isTurnToMove(id2));
+        assertTrue(session.isTurnToMove(id1));
+
+        session.makeMove(id2, move2);
+
+        Move move3 = new Move(13, 52);
+        session.makeMove(id1, move3);
+        assertFalse(session.isTurnToMove(id1));
+        assertTrue(session.isTurnToMove(id2));
+
+        //can't place chip in same position twice
+        session.makeMove(id2, move2);
+        assertTrue(session.isTurnToMove(id2));
+        assertFalse(session.isTurnToMove(id1));
+
+        //can't move piece to same position
+        session.makeMove(id2, 67);
+        assertTrue(session.isTurnToMove(id2));
+        assertFalse(session.isTurnToMove(id1));
+
+        //cam't move out of turn
+        session.makeMove(id1, 22);
+    }
+
+    public void testCanJumpOver(){
+        assertTrue(false);
+    }
+
+    public void testCanJumpDiagonally(){
+        assertTrue(false);
     }
 
     public void testGetMoveStatus(){
-
+        assertTrue(false);
     }
 
     public void testIsValidMove(){
-
+        assertTrue(false);
     }
 
     public void testMoveForward(){
-
+        assertTrue(false);
     }
 
     public void testMoveBackward(){
-
+        assertTrue(false);
     }
 
     public void testMoveLeft(){
-
+        assertTrue(false);
     }
 
     public void testMoveRight(){
-
+        assertTrue(false);
     }
 
     public void testJumpOver(){
-
+        assertTrue(false);
     }
 
     public void testMoveDiagonally(){
+        assertTrue(false);
     }
 
     public void testCheckForVictory(){
-
+        assertTrue(false);
     }
 }
--- app/src/main/java/com/games/jclundy/quoridor/GameRules/Session.java
+++ app/src/main/java/com/games/jclundy/quoridor/GameRules/Session.java
@@ -5,6 +5,111 @@ package com.games.jclundy.quoridor.GameRules;
  */
 public class Session {
 
+    protected int numPlayers;
+    protected Board board;
+    protected Player[] playerList;
+    private int currentPlayerID;
+
+    public Session(int numberOfPlayers){
+        numPlayers = (numberOfPlayers >= 2 && numberOfPlayers <= 4)? numberOfPlayers : 2;
+        board = new Board(numPlayers);
+        initializePlayers();
+        currentPlayerID = playerList[0].id;
+    }
+
+    private void initializePlayers(){
+        playerList = new Player[numPlayers];
+        int numberOfChips = GameRuleConstants.TOTAL_WALL_NUM/numPlayers;
+        for(int i = 0; i < playerList.length; i++){
+            playerList[i] = new Player(i, GameRuleConstants.START_POSITIONS[i],numberOfChips);
+        }
+    }
+
+    public int getMoveStatus(int playerID, Move move){
+        return -1;
+    }
+
+    public boolean isMoveValid(){
+        return false;
+    }
+
+    boolean isTurnToMove(int playerID){
+        return playerID == currentPlayerID;
+    }
+
+    int getPlayerPosition(int playerID){
+        return board.getPlayerPosition(playerID);
+    }
+
+    void makeMove(int id, Move move){
+
+    }
+
+    void makeMove(int id, int squareNum){
+
+    }
+
+    boolean canJumpOver(int id, int squareNum){
+        //squareNum is 2 rows XOR 2 columns away
+        //another player occupies an adjacent square
+        //the squareNum is adjacent to the player's occupying square
+
+        int currentPosition = getPlayerPosition(id);
+        int col = Board.getCol(currentPosition);
+        int row = Board.getRow(currentPosition);
+        int toCol = Board.getCol(squareNum);
+        int toRow = Board.getRow(squareNum);
+
+        int occupierOfToSquare = board.getOccupierAtSquare(squareNum);
+
+        int colDiff = Math.abs(row - toRow);
+        int rowDiff = Math.abs(col - toCol);
+        int occupier;
+        if(colDiff == 1 && rowDiff == 0){
+            occupier = board.getOccupierAtSquare(Board.getSquareNum(toCol, row));
+            return occupier != GameRuleConstants.EMPTY && occupierOfToSquare == GameRuleConstants.EMPTY;
+        }
+        else if (colDiff == 0 && rowDiff == 1){
+            occupier = board.getOccupierAtSquare(Board.getSquareNum(col, toRow));
+            return occupier != GameRuleConstants.EMPTY && occupierOfToSquare == GameRuleConstants.EMPTY;
+        }
+        else
+            return false;
+    }
+
+    boolean canJumpDiagonally(int id, int squareNum){
+        //squareNum is in adjacency set of current square
+        //square Num is in a different row and different column
+        //another player occupies the square in adjacent column XOR adjacent row
+        //square occupied by above player is adjacent to squareNum
+
+        /*
+
+         __ __ [ ]
+        [ ][P][X]
+        [ ][ ][ ]
+
+         */
+
+        int currentPosition = getPlayerPosition(id);
+        int col = Board.getCol(currentPosition);
+        int row = Board.getRow(currentPosition);
+        int toCol = Board.getCol(squareNum);
+        int toRow = Board.getRow(squareNum);
+
+        int rowDiff = Math.abs(row - toRow);
+        int colDiff = Math.abs(col - toCol);
+        if (rowDiff + colDiff == 2 && board.squares[currentPosition].isInAdjacencySet(squareNum)){
+            int horizontalSquare = Board.getSquareNum(toCol, row);
+            int verticalSquare = Board.getSquareNum(col, toRow);
+
+            boolean validMove = board.getOccupierAtSquare(horizontalSquare) != GameRuleConstants.EMPTY &&
+                    board.squares[verticalSquare].isAdjacent(squareNum) && !board.squares[verticalSquare].isInAdjacencySet(squareNum);
+            return validMove;
+        }
+        return false;
+    }
+
     public void moveForward(int playerID){
 
     }
