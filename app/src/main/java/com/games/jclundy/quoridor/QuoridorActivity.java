de955ad7a092b7bd99c70fb03f2571b94acb42ea
Joseph Lundy <joseph.lundy456@gmail.com>
2014-11-25 19:45:49 -0500

    refactors QuoridorActivity, adds functions to handle ui state after a move

--- app/src/main/java/com/games/jclundy/quoridor/QuoridorActivity.java
+++ app/src/main/java/com/games/jclundy/quoridor/QuoridorActivity.java
@@ -2,16 +2,14 @@ package com.games.jclundy.quoridor;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
-import android.widget.Toast;
 
 import com.games.jclundy.quoridor.GameRules.Board;
 import com.games.jclundy.quoridor.GameRules.GameRuleConstants;
@@ -21,71 +19,37 @@ import com.games.jclundy.quoridor.board.SquaresTableView;
 import java.util.HashMap;
 
 
-public class QuoridorActivity extends Activity {
+public class QuoridorActivity extends Activity
+{
     private SquaresTableView squaresTable;
     private Button up;
     private Button down;
     private Button left;
     private Button right;
-    private HashMap<Integer, Integer> playerTurns;
+    private HashMap<Integer, Integer> playerImageMap;
     private Session session;
     private Switch toggle;
     private int numPlayers;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quoridor);
         Intent previous = getIntent();
         numPlayers = previous.getIntExtra(getString(R.string.NUM_PLAYERS), 2);
 
-        squaresTable = (SquaresTableView) findViewById(R.id.squaresTable);
-        squaresTable.setOnTouchListener( new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                int x = (int) event.getX();
-                int y = (int) event.getY();
-                int action = event.getAction();
-                if(action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE) {
-                    boolean isVertical = toggle.isChecked();
-                    int position = squaresTable.getPosition(x, y);
-                    position = Board.getValidSquareForWall(position);
-                    if(session.canPlaceWall(position, isVertical)){
-
-                        int currentPlayerPosition = session.getCurrentPlayerPosition();
-                        squaresTable.unHighlightSquare(currentPlayerPosition);
-
-                        session.placeWall(position, isVertical);
-                        squaresTable.placeWall(position, isVertical);
-
-                        int nextPlayerPosition = session.getCurrentPlayerPosition();
-                        squaresTable.highlightSquare(nextPlayerPosition);
-                    }
-                    return true;
-                }
-
-                return false;
-            }
-        });
         createBoard();
-        up = (Button) findViewById(R.id.up);
-        down = (Button) findViewById(R.id.down);
-        left = (Button) findViewById(R.id.left);
-        right = (Button) findViewById(R.id.right);
-
-        View.OnClickListener listener = buttonListener();
-        up.setOnClickListener(listener);
-        down.setOnClickListener(listener);
-        right.setOnClickListener(listener);
-        left.setOnClickListener(listener);
+        createButtons();
+        setButtonsListener(buttonListener());
 
         session = new Session(numPlayers);
 
-        playerTurns = new HashMap<Integer, Integer>();
-        playerTurns.put(GameRuleConstants.PLAYER_IDS[0], R.drawable.bluecircle);
-        playerTurns.put(GameRuleConstants.PLAYER_IDS[1], R.drawable.orangecircle);
-        playerTurns.put(GameRuleConstants.PLAYER_IDS[2], R.drawable.greencircle);
-        playerTurns.put(GameRuleConstants.PLAYER_IDS[3], R.drawable.redcircle);
+        playerImageMap = new HashMap<Integer, Integer>();
+        playerImageMap.put(GameRuleConstants.PLAYER_IDS[0], R.drawable.bluecircle);
+        playerImageMap.put(GameRuleConstants.PLAYER_IDS[1], R.drawable.orangecircle);
+        playerImageMap.put(GameRuleConstants.PLAYER_IDS[2], R.drawable.greencircle);
+        playerImageMap.put(GameRuleConstants.PLAYER_IDS[3], R.drawable.redcircle);
 
         toggle = (Switch) findViewById(R.id.toggle);
     }
@@ -108,32 +72,73 @@ public class QuoridorActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
-        }
+
+    private View.OnTouchListener generateBoardListener()
+    {
+        return new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                int action = event.getAction();
+                if(action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE)
+                {
+                    boolean isVertical = toggle.isChecked();
+                    int position = squaresTable.getPosition(x, y);
+                    position = Board.getValidSquareForWall(position);
+                    if(session.canPlaceWall(position, isVertical))
+                    {
+                        int currentPlayerPosition = session.getCurrentPlayerPosition();
+                        squaresTable.unHighlightSquare(currentPlayerPosition);
+
+                        session.placeWall(position, isVertical);
+                        squaresTable.placeWall(position, isVertical);
+
+                        int nextPlayerPosition = session.getCurrentPlayerPosition();
+                        squaresTable.highlightSquare(nextPlayerPosition);
+                    }
+                    return true;
+                }
+                return false;
+            }
+        };
     }
 
-    public void clearBoard() {
+    public void clearBoard()
+    {
         squaresTable.removeAllViews();
         squaresTable.invalidate();
         createBoard();
     }
 
-    private void createBoard() {
+    private void createBoard()
+    {
+        squaresTable = (SquaresTableView) findViewById(R.id.squaresTable);
+        squaresTable.setOnTouchListener(generateBoardListener());
         int squareSize = 50;
         squaresTable.disposeSquares(500, 500, squareSize, numPlayers);
+    }
+
+    private void createButtons()
+    {
+        up = (Button) findViewById(R.id.up);
+        down = (Button) findViewById(R.id.down);
+        left = (Button) findViewById(R.id.left);
+        right = (Button) findViewById(R.id.right);
+    }
+
+    private void setButtonsListener(View.OnClickListener listener)
+    {
+        up.setOnClickListener(listener);
+        down.setOnClickListener(listener);
+        right.setOnClickListener(listener);
+        left.setOnClickListener(listener);
     }
 
     private View.OnClickListener buttonListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int currentPlayer = session.getCurrentPlayerID();
                 int currentPlayerPosition = session.getCurrentPlayerPosition();
                 int newPosition = currentPlayerPosition;
                 switch (view.getId()) {
@@ -152,14 +157,52 @@ public class QuoridorActivity extends Activity {
                     default:
                         break;
                 }
-                session.makeMove(newPosition);
-                if(session.getCurrentPlayerID() != currentPlayer){
-                    int playerImg = playerTurns.get(currentPlayer);
-                    squaresTable.movePawn(session.getPlayerPosition(currentPlayer), playerImg);
-                    int nextPlayerPosition = session.getCurrentPlayerPosition();
-                    squaresTable.highlightSquare(nextPlayerPosition);
-                }
+                handleMove(newPosition);
             }
         };
+    }
+
+    private void handleMove(int move)
+    {
+        int currentPlayer = session.getCurrentPlayerID();
+        if(session.isMoveValid(move))
+        {
+            session.makeMove(move);
+            int playerImg = playerImageMap.get(currentPlayer);
+            squaresTable.movePawn(session.getPlayerPosition(currentPlayer), playerImg);
+            handleGameStatus(currentPlayer);
+        }
+        else
+        {
+            handleInvalidMove();
+        }
+    }
+
+    private void handleGameStatus(int lastPlayerToMove)
+    {
+        if(session.playerHasWon(lastPlayerToMove))
+            handleVictory(lastPlayerToMove);
+        else
+            handleNextMove();
+    }
+
+    private void handleVictory(int winningPlayer)
+    {
+        squaresTable.highlightSquare(session.getPlayerPosition(winningPlayer));
+        squaresTable.setOnTouchListener(null);
+        setButtonsListener(null);
+        Log.d("VICTORY", "Game is Over");
+    }
+
+    private void handleNextMove()
+    {
+        int nextPlayerPosition = session.getCurrentPlayerPosition();
+        squaresTable.highlightSquare(nextPlayerPosition);
+        Log.d("VALID MOVE", "Next player to move");
+    }
+
+    private void handleInvalidMove()
+    {
+        Log.d("INVALID MOVE", "Please redo move");
     }
 }
